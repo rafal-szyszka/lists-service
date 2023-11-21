0889f682af3e867e5728b98d890855ecd0650c3a
Mateusz Wojtas <mateusz.wojtas@bpower2.com>
2023-11-21 11:50:03 +0000

    2898892 - list filtering

--- src/main/kotlin/com/bpower2/listservice/api/queries/QueriesService.kt
+++ src/main/kotlin/com/bpower2/listservice/api/queries/QueriesService.kt
@@ -2,29 +2,37 @@ package com.bpower2.listservice.api.queries
 
 import com.bpower2.listservice.api.queries.data.ListWithData
 import com.bpower2.listservice.communication.data_service.api.DataServiceBridgeService
+import com.bpower2.listservice.communication.forms_service.api.FormsServiceBridgeService
+import com.bpower2.listservice.communication.forms_service.api.data.FormDefinition
 import com.bpower2.listservice.core.data.models.SimpleList
 import com.bpower2.listservice.core.data.repos.ListRepository
+import com.bpower2.listservice.proql.ProQLQuery
+import com.bpower2.listservice.proql.ProQLSubQuery
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class QueriesService(
     private val listRepository: ListRepository,
-    private val dataServiceBridgeService: DataServiceBridgeService
+    private val dataServiceBridgeService: DataServiceBridgeService,
+    private val formsServiceBridgeService: FormsServiceBridgeService
 ) {
 
     fun getList(id: String): Optional<SimpleList> {
         return listRepository.findById(id)
     }
 
-    fun getListWithData(id: String, page: Optional<Int>): Optional<ListWithData> {
+    fun getListWithData(id: String, page: Optional<Int>, filter: Optional<Map<String, Any>>): Optional<ListWithData> {
         val list = listRepository.findById(id)
         if (list.isEmpty) return Optional.empty()
+        var query = list.get().query!!
+        val formStructure = formsServiceBridgeService.getFormDefinition(list.get().presenterFormId!!)
 
-        val query = list.get().query!!
+        filter.ifPresent {
+            filterList(filter, formStructure, query)
+        }
 
         page.ifPresent { query.page = page.get() }
-
         val data = dataServiceBridgeService.getData(query)
 
         return Optional.of(
@@ -32,4 +40,22 @@ class QueriesService(
         )
     }
 
+    private fun filterList(
+        filter: Optional<Map<String, Any>>,
+        formStructure: FormDefinition,
+        query: ProQLQuery
+    ) {
+        filter.get().forEach { (key, value) ->
+            val field = formStructure.fields!!.find { it.id == key }
+            val persistentData = field!!.persistenceData!!
+            val type = persistentData.type!!
+            val name = persistentData.name!!
+            if (type.primitive == true) {
+                query.properties = mutableMapOf(name to value)
+            } else {
+                query.subQueries =
+                    listOf(ProQLSubQuery(type.module + "." + type.name, mutableMapOf("id" to value), null, name))
+            }
+        }
+    }
 }
